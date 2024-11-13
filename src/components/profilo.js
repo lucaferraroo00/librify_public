6c80e9d325323a044158605a51a27ed35b6f08ec
lucaferraroo00 <luca.ferraro2b@gmail.com>
2024-11-13 15:36:39 +0100

    add new nav bar to profilo

--- src/components/profilo.js
+++ src/components/profilo.js
@@ -1,37 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiBlackBook } from "react-icons/gi";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { BrowserRouter as Router, useNavigate, Navigate} from 'react-router-dom';
 import lillo_img from "./lillo_profilo.jpg";
+import { CiMenuBurger } from "react-icons/ci";
+import { BsHouseDoor } from "react-icons/bs";
+
 import "./styles.css";
 
 function Profilo(props) {
+    const [activeMenu, setActiveMenu] = useState(false);
 
     const navigate = useNavigate();
 
     const handleClickHome = () => {
         navigate('/')
     }
+    const handleClickMenu = () => setActiveMenu(prevState => !prevState);
+
+    
+  const handleNavigateProfilo = () => {
+    navigate('/profilo')
+  }
+
+  const handleNavigateEventi = () => {
+    navigate('/eventi')
+  }
+
+  const handleNavigateCrea= () => {
+    navigate('/crea')
+  }
+
 
     return (
         <>
-        <Navbar
-            bg="primary"
-            className="d-flex justify-content-center align-items-center fixed-top"
-        >
-            <Button variant="light" className="ml-2" onClick={handleClickHome}
-            >Home</Button>
-            <div className="flex-grow-1 text-center">
-            {/* Center the logo and title */}
-            <GiBlackBook style={{ fontSize: "2.5rem" }} />{" "}
-            {/* Adjust font size as needed */}
-            <h1 className="d-inline ml-2 text-white">Librify</h1>{" "}
-            {/* Add margin-left and change text color */}
-            </div>
-        </Navbar>
-        <Container fluid className="navbar-spacing">
+    <Container fluid>
+    <Row className="d-flex align-items-center sticky-sm-top bg-primary">
+      <Col className="m-1">
+        <CiMenuBurger
+          className=""
+          style={{ fontSize: '2.1rem', color:"white", cursor:"pointer"}} 
+          onClick={handleClickMenu}></CiMenuBurger>
+        </Col>
+        <Col className="d-flex align-items-center justify-content-center">
+          <GiBlackBook style={{ fontSize: '2.1rem', color:"white" }} />
+          <h1 className="text-white">Librify</h1>
+          <GiBlackBook style={{ fontSize: '2.1rem', color:"white" }} />
+        </Col>
+        <Col className="d-flex d-flex justify-content-end m-1">
+        <BsHouseDoor
+          className=""
+          style={{ fontSize: '2.1rem', color:"white", cursor:"pointer"}} 
+          onClick={handleClickHome}></BsHouseDoor>
+        </Col>
+      </Row>
+      <Row>
+        {activeMenu && (
+          <Col className="bg-secondary text-white d-flex flex-column vh-100" >
+            <Row 
+            style={{ cursor: 'pointer' }}
+            onClick={handleNavigateProfilo} >
+              <Col className="d-flex justify-content-center  pt-4"> <h3>Profilo</h3></Col>
+            </Row>
+            <Row
+            style={{ cursor: 'pointer' }}
+            onClick={handleNavigateEventi} >
+            <Col className="d-flex justify-content-center  pt-4"> <h3>Eventi</h3></Col>
+            </Row>
+            <Row
+            style={{ cursor: 'pointer' }}
+            onClick={handleNavigateCrea} >
+            <Col className="d-flex justify-content-center pt-4">  <h3>Crea</h3></Col>
+            </Row>
+          </Col>
+        )}
+        <Col {...(activeMenu ? { md: 10 } : {})}>
         <Row className="d-flex justify-content-center">
             <img
               src={lillo_img}
@@ -56,6 +101,8 @@ function Profilo(props) {
             <h2 className="d-flex justify-content-center">SEGUITI: 89</h2>
             </Col>
           </Row>
+        </Col>
+        </Row>
         </Container>
         </>
     );
