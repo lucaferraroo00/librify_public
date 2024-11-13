173c051d23638eca72cf7e410f82ffc28c55d7be
lucaferraroo00 <luca.ferraro2b@gmail.com>
2024-11-13 17:04:31 +0100

    add new navbar to crea

--- src/components/crea.js
+++ src/components/crea.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiBlackBook } from "react-icons/gi";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Container, Row, Col } from "react-bootstrap";
@@ -12,32 +12,76 @@ import ReactSearchBox from "react-search-box";
 import "./styles.css";
 import { BsFillPlusCircleFill, BsSearch } from "react-icons/bs";
 import norw_wood from "./norwegian_wood.jfif"
+import { CiMenuBurger } from "react-icons/ci";
+import { BsHouseDoor } from "react-icons/bs";
 
 function Crea(props) {
+  const [activeMenu, setActiveMenu] = useState(false);
+
   const navigate = useNavigate();
 
   const handleClickHome = () => {
     navigate("/");
   };
 
+  const handleClickMenu = () => setActiveMenu(prevState => !prevState);
+
+    
+  const handleNavigateProfilo = () => {
+    navigate('/profilo')
+  }
+
+  const handleNavigateEventi = () => {
+    navigate('/eventi')
+  }
+
+  const handleNavigateCrea= () => {
+    navigate('/crea')
+  }
+
   return (
-    <>
-      <Navbar
-        bg="primary"
-        className="d-flex justify-content-center align-items-center fixed-top"
-      >
-        <Button variant="light" className="ml-2" onClick={handleClickHome}>
-          Home
-        </Button>
-        <div className="flex-grow-1 text-center ">
-          {/* Center the logo and title */}
-          <GiBlackBook style={{ fontSize: "2.1rem" }} />
-          {/* Adjust font size as needed */}
-          <h1 className="d-inline ml-2 text-white">Librify</h1>{" "}
-          {/* Add margin-left and change text color */}
-        </div>
-      </Navbar>
-      <Container fluid className="navbar-spacing mb-4">
+    <Container fluid>
+      <Row className="d-flex align-items-center sticky-sm-top bg-primary">
+      <Col className="m-1">
+        <CiMenuBurger
+          className=""
+          style={{ fontSize: '2.1rem', color:"white", cursor:"pointer"}} 
+          onClick={handleClickMenu}></CiMenuBurger>
+        </Col>
+        <Col className="d-flex align-items-center justify-content-center">
+          <GiBlackBook style={{ fontSize: '2.1rem', color:"white" }} />
+          <h1 className="text-white">Librify</h1>
+          <GiBlackBook style={{ fontSize: '2.1rem', color:"white" }} />
+        </Col>
+        <Col className="d-flex d-flex justify-content-end m-1">
+        <BsHouseDoor
+          className=""
+          style={{ fontSize: '2.1rem', color:"white", cursor:"pointer"}} 
+          onClick={handleClickHome}></BsHouseDoor>
+        </Col>
+      </Row>
+        <Row fluid>
+        {activeMenu && (
+          <Col className="bg-secondary text-white d-flex flex-column" >
+            <Row 
+            style={{ cursor: 'pointer' }}
+            onClick={handleNavigateProfilo} >
+              <Col className="d-flex justify-content-center  pt-4"> <h3>Profilo</h3></Col>
+            </Row>
+            <Row
+            style={{ cursor: 'pointer' }}
+            onClick={handleNavigateEventi} >
+            <Col className="d-flex justify-content-center  pt-4"> <h3>Eventi</h3></Col>
+            </Row>
+            <Row
+            style={{ cursor: 'pointer' }}
+            onClick={handleNavigateCrea} >
+            <Col className="d-flex justify-content-center pt-4">  <h3>Crea</h3></Col>
+            </Row>
+          </Col>
+        )}
+        <Col {...(activeMenu ? { md: 10 } : {})}>
+      <Container fluid className="mb-4">
         <Row className="pt-4">
           <Col className="d-flex justify-content-center">
             <h1 style={{ color: "#33c1ff", textDecoration: "underline" }}>
@@ -76,7 +120,9 @@ function Crea(props) {
         </Row>
         
       </Container>
-    </>
+      </Col>
+      </Row>
+    </Container>
   );
 }
 
